d30c739cae76bea515fa5d02fe2a92f0e90760ba
473113 <473113@PC215561.cts.com>
2019-04-10 16:09:59 +0530

    update

--- GitTesting_Saran/src/WebPage/Page1.java
+++ GitTesting_Saran/src/WebPage/Page1.java
@@ -5,7 +5,8 @@ public class Page1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("InstagramPage");
+		System.out.println("InstagramPage1");
+		
 		
 	}
 
