a3711c89152f10104265fca0f9e2815d57c6fa7e
473113 <473113@PC215561.cts.com>
2019-04-12 11:01:26 +0530

    Update4

--- GitTesting_Saran/src/WebPage/Page1.java
+++ GitTesting_Saran/src/WebPage/Page1.java
@@ -5,9 +5,21 @@ public class Page1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("InstagramPage2");
+		System.out.println("InstagramPage1");
 		
-		System.out.println("InstagramGram");
+		System.out.println("InstagramPage2");
+
+	       // method call
+	       myMethod();
+	       
+	       System.out.println("Method was executed successfully!");
+	   }
+
+	   private static void myMethod(){
+			   
+	       System.out.println("Print My Method");
+	   }
+	
 	}
 
-}
+
